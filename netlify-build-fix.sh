916e8334b84cbb965eb5f479acbe9872459292af
Mark Scott <mark.scott.phx@gmail.com>
2019-12-11 11:36:30 -0700

    no message

--- netlify-build-fix.sh
+++ netlify-build-fix.sh
@@ -13,10 +13,10 @@ echo "Hugo version: ${HUGO_VERSION}"
 rm -fr *.deb
 
 echo "downloading libstdc..."
-wget --no-clobber -q http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb || exit 1
+wget --no-clobber -q http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb || exit 0
 
 echo "downloading extended hugo..."
-wget --no-clobber -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_FLAVOUR}_${HUGO_VERSION}_Linux-64bit.deb || exit 1
+wget --no-clobber -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_FLAVOUR}_${HUGO_VERSION}_Linux-64bit.deb || exit 0
 
 grep "hugo=" ~/.bashrc || echo "alias hugo='LD_LIBRARY_PATH=$(pwd)/tmp/usr/lib/x86_64-linux-gnu $(pwd)/tmp/usr/local/bin/hugo'" >> ~/.bashrc
 find . -name '*.deb' -exec dpkg -x {} $(pwd)/tmp \;
